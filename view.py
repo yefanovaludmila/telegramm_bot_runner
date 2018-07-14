881c59da2a9bda975b11c1a3897f0928b11e159b
yefanovaludmila <yefanovaludmila@gmail.com>
2018-07-14 16:55:59 +0300

    enother logic

--- view.py
+++ view.py
@@ -1,28 +1,62 @@
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, ForceReply
-import telepot
-import datetime
 import time
 
 class View:
-    d = {}
+    d_stop = {}
+    d_start ={}
+    d_circle = []
+    d_runner = []
     def __init__(self,bot):
         self.bot = bot
 
-    def root_handle(self,msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        markup = ReplyKeyboardMarkup(keyboard=[['Start', 'Stop']])
-        if (msg['text'] == "Start" or msg['text'] == "Stop"):
+    def start_stop_run(self, text, date):
+        d_start_stop = {text: date}
+        return d_start_stop
 
-            self.bot.sendMessage(chat_id, f"{msg['text']} time is "
-                                     f"{datetime.datetime.fromtimestamp(msg['date']).strftime('%Y-%m-%d %H:%M:%S')}",
-                            reply_markup=markup)
-            self.d[msg['text']] = msg['date']
+    def unresolved_choice(self, msg):
+        markup = ReplyKeyboardMarkup(keyboard=[['Start', 'Circle'], ['Stop']])
+        self.bot.sendMessage(msg['chat']['id'], msg['chat']['first_name'] + ', please choose button Start or Stop',
+                             reply_markup=markup)
+
+    def root_handle(self,msg):
+        print(msg['date'])
+        markup = ReplyKeyboardMarkup(keyboard=[['Start', 'Circle'], ['Stop']])
+        if (msg['text'] == "Start"):
+            self.d_start = View.start_stop_run(self,msg['text'], msg['date'])
+        elif (msg['text'] == "Circle"):
+            if (self.d_start.get("Start", 0) > 0):
+                run_circle = View.start_stop_run(self, msg['text'], msg['date'])
+                self.d_circle.append(run_circle)
+            else:
+                self.bot.sendMessage(msg['chat']['id'], f"first press the \'Start\' button",reply_markup=markup)
+        elif (msg['text'] == "Stop"):
+            self.d_stop = View.start_stop_run(self, msg['text'], msg['date'])
+            self.d_runner.append([{"Start":self.d_start},{"Circle":self.d_circle},{"Stop":self.d_stop},{"User":msg['chat']['first_name']}])
+            if(self.d_start.get("Start", 0)>0):
+                run_time = self.d_stop.get("Stop", 0) - self.d_start.get("Start", 0)
+                new_run_time = time.strftime('%H:%M:%S', time.gmtime(run_time))
+                self.bot.sendMessage(msg['chat']['id'], f'run time is: {new_run_time}', reply_markup=markup)
+
+                self.bot.sendMessage(msg['chat']['id'],f'count of circles: {len(self.d_circle)+1}')
+                self.d_stop = {}
+                self.d_start = {}
+                self.d_circle = []
+            else:
+                self.bot.sendMessage(msg['chat']['id'], f"first press the \'Start\' button",reply_markup=markup)
         else:
-            self.bot.sendMessage(chat_id, msg['chat']['first_name'] + ', please choose button Start or Stop', reply_markup=markup)
-        if (msg['text'] == "Stop"):
-            run_time = self.d.get("Stop", 0) - self.d.get("Start", 0)
-            new_run_time = time.strftime('%H:%M:%S', time.gmtime(run_time))
-            srt_run_time = msg['chat'][
-                               'first_name'] + f', your run time is {new_run_time}. You are the best runner in the world!!!'
-            self.bot.sendMessage(chat_id, srt_run_time, reply_markup=markup)
-        
+            View.unresolved_choice(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
