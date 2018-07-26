6a38df194a9fd128f0204369f7354f7a5b370ecd
yefanovaludmila <yefanovaludmila@gmail.com>
2018-07-26 13:41:40 +0300

    version 2.0 change token

--- run.py
+++ run.py
@@ -8,27 +8,29 @@ from utils import Utils
 from view import View
 
 
-def setup_logger(logfile, level):
-    logger.setLevel(logging.DEBUG)
-
-    fh = logging.FileHandler(logfile)
-    fh.setLevel(logging.ERROR)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-
-
-
-parser = argparse.ArgumentParser(description='Some initial bot.')
-parser.add_argument('--config', type=str,
-                    help='A config fole with required params',
-                    required=True)
-args = parser.parse_args()
-
-logger = logging.getLogger('telegramm_bot')
-
-uti = Utils(args.config)
-TOKEN = uti.get_token()
+# def setup_logger(logfile, level):
+#     logger.setLevel(logging.DEBUG)
+#
+#     fh = logging.FileHandler(logfile)
+#     fh.setLevel(logging.ERROR)
+#
+#     ch = logging.StreamHandler()
+#     ch.setLevel(level)
+#
+#
+#
+# parser = argparse.ArgumentParser(description='Some initial bot.')
+# parser.add_argument('--config',
+#                     type=str,
+#                     help='A config fole with required params',
+#                     required=True)
+# args = parser.parse_args()
+#
+# logger = logging.getLogger('telegramm_bot')
+#
+# uti = Utils(args.config)
+# TOKEN = uti.get_token()
+TOKEN = '415996818:AAFUiHtSM8Y7nkpSflHzfT9zPvpPxrk5jwk'
 
 bot = telepot.Bot(TOKEN)
 viwer = View(bot)
